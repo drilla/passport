2f50f56292cb569f9ddb8dba04be8f99e7c2a952
drilla <drillerok@gmail.com>
2018-06-23 14:44:08 +0300

    added phpunit

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Http\Response;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use mikehaertl\pdftk\Pdf;
 
 class Controller extends BaseController
 {
@@ -13,5 +15,24 @@ class Controller extends BaseController
 
     public function show() {
         return view('welcome');
+    }
+
+    public function generateFile() {
+        $data = [
+            'PGSNumber' => "дело хуйsdf dsf"
+        ];
+
+        $pdf = new Pdf('/home/drilla/Loads/form_zp.pdf');
+
+
+        $pdf
+            ->needAppearances()
+            ->fillForm($data)
+        ;
+
+        $pdf->saveAs('/tmp/tmp_form.pdf');
+
+
+        return \response()->file('/tmp/tmp_form.pdf', 'hell.pdf');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'Controller@show');
+
+Route::get('/test', 'Controller@generateFile');
